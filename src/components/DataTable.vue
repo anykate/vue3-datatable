7a484f0351eadf64fb9776d08bd44ec715c93511
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-10-16 18:07:47 +0530

    Adding logic for SearchForm component

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,14 +1,52 @@
 <script setup>
-defineProps({
+import FilterDropdown from '@/components/FilterDropdown.vue'
+import FilterRadios from '@/components/FilterRadios.vue'
+import SearchForm from '@/components/SearchForm.vue'
+import { computed, ref } from 'vue'
+
+const searchFilter = ref('')
+
+const props = defineProps({
     items: {
         type: Array,
         required: true,
     },
 })
+
+const filteredItems = computed(() => {
+    if (searchFilter.value.trim().length === 0) return props.items
+
+    return props.items.filter(
+        (item) =>
+            item.title
+                .toLowerCase()
+                .includes(searchFilter.value.trim().toLowerCase()) ||
+            item.user.name
+                .toLowerCase()
+                .includes(searchFilter.value.trim().toLowerCase())
+    )
+})
+
+const handleSearch = (search) => {
+    searchFilter.value = search
+}
 </script>
 
 <template>
     <div class="relative rounded-lg border bg-white">
+        <div class="flex items-center justify-between">
+            <!-- Search bar -->
+            <SearchForm @search="handleSearch" />
+            <div class="text-2xl font-extrabold">
+                {{ filteredItems.length }}
+            </div>
+            <div class="flex items-center justify-end text-sm font-semibold">
+                <!-- Radio buttons -->
+                <FilterRadios />
+                <!-- List of filters for statuses -->
+                <FilterDropdown />
+            </div>
+        </div>
         <table class="w-full text-left text-sm text-gray-500">
             <thead class="bg-gray-50 text-xs uppercase text-gray-700">
                 <tr>
@@ -23,7 +61,7 @@ defineProps({
                 </tr>
             </thead>
             <tr
-                v-for="(item, index) in items"
+                v-for="item in filteredItems"
                 :key="item.id"
                 class="border-b"
             >
@@ -46,8 +84,9 @@ defineProps({
                     <router-link
                         :to="{ name: 'home' }"
                         class="text-indigo-500 hover:underline"
-                        >Details</router-link
                     >
+                        Details
+                    </router-link>
                 </td>
             </tr>
         </table>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,28 @@
+<script setup>
+const emit = defineEmits(['search'])
+
+const search = (e) => {
+    emit('search', e.target.value)
+}
+</script>
+
+<template>
+    <form class="flex items-center px-4 py-3">
+        <label
+            for="search"
+            class="sr-only"
+            >Search</label
+        >
+        <div class="relative w-full">
+            <input
+                type="text"
+                placeholder="Search..."
+                id="search"
+                @input="search"
+                class="rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900"
+            />
+        </div>
+    </form>
+</template>
+
+<style scoped></style>
